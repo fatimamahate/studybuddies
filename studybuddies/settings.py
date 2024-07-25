41e0d17b1951653dc870defbb97fa0c6492a2146
fatimamahate <fatimamahate@yahoo.co.uk>
2024-07-25 14:52:24 +0000

    secure django database

--- studybuddies/settings.py
+++ studybuddies/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t$+_s!v=19g$7!e@d%-(f_veg=da^1t)1)z((a(qo&rwo8e@w&'
+SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
