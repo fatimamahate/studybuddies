9441cabe04047c6d1efbecd1916efcbf7c4d0e05
fatimamahate <fatimamahate@yahoo.co.uk>
2024-07-28 11:05:04 +0000

    Create post_detail.html which is for each post. Add css styling. Add default pencils image

--- studyexamtips/views.py
+++ studyexamtips/views.py
@@ -5,7 +5,7 @@
 # def sb_blog(request):
 #     return HttpResponse("Hello World!")
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -14,3 +14,26 @@ class PostList(generic.ListView):
     # template_name = "post_list.html"
     template_name = "studyexamtips/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "studyexamtips/post_detail.html",
+        {"post": post},
+    )
