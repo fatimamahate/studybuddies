1743f0ba909ff0b658965b8399c7766440a96957
fatimamahate <fatimamahate@yahoo.co.uk>
2024-07-31 14:34:24 +0000

    add image field to model and allow for images to be added in

--- studyexamtips/models.py
+++ studyexamtips/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -10,6 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="studyexam_posts")
+    featured_image = CloudinaryField('image', default='placeholder')
     main_body = models.TextField()
     preview = models.TextField(blank=True)
     post_created_on = models.DateTimeField(auto_now_add=True)
@@ -24,7 +26,7 @@ class Post(models.Model):
         ordering = ["-post_created_on"]
     
     def __str__(self):
-        return f"{self.title} by {self.author}. Diffulty:{self.difficulty}"
+        return f"{self.title} by {self.author}."
 
 
 class Comment(models.Model):
