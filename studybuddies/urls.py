8baa6bebc4292ac41493e5c8bece075315c8cec0
fatimamahate <fatimamahate@yahoo.co.uk>
2024-08-01 14:27:57 +0000

    custom 404

--- studybuddies/urls.py
+++ studybuddies/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
 # from studyexamtips import views
@@ -29,5 +30,5 @@ urlpatterns = [
 ]
 
 
-
+handler404 = 'studyexamtips.views.my_404'
 
